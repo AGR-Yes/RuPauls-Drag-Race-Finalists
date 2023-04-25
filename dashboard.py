f6f041034d1df1cfbd91e2947011c12e1c9b10e8
Anton Reyes <anton_reyes@dlsu.edu.ph>
2023-04-26 01:00:23 +0800

    dashboard attempt

--- dashboard.py
+++ dashboard.py
@@ -7,49 +7,21 @@ import plotly.express as px
 from dash.dependencies import Input, Output
 
 # load data
-df1 = pd.read_csv('data/main.csv')
-df2 = pd.read_csv('data/score.csv')
+df1 = pd.read_csv('dash_data/placements.csv')
+#df2 = pd.read_csv('data_data/scores.csv')
+
 # make plot
 
 # initialize app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.UNITED])
 # set app layout
 
-app.layout = html.Div(
-    children=[
-        html.H1(children="Avocado Analytics"),
-        html.P(
-            children=(
-                "Analyze the behavior of avocado prices and the number"
-                " of avocados sold in the US between 2015 and 2018"
-            ),
-        ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": df1["Placement"],
-                        "y": df1["count"],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "Average Price of Avocados"},
-            },
-        ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": df2["code"],
-                        "y": df2["count"],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "Avocados Sold"},
-            },
-        ),
-    ]
-)
+html.Div([
+    html.H1('Hello Dash'),
+    html.Div([
+        html.P('This is a paragraph.')
+    ])
+])
 
 
 
