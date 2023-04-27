cfe4a30a0ae810d9e94e8f552a82c4b15ce0006d
Anton Reyes <anton_reyes@dlsu.edu.ph>
2023-04-27 21:49:46 +0800

    Added scatter and line plots
    Need to fix formatting of dropdowns

--- dashboard.py
+++ dashboard.py
@@ -1,10 +1,16 @@
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
+import numpy as np
+
+placement = pd.read_csv("dash_data\placements.csv")
+scores = pd.read_csv("dash_data\scores.csv")
 
 app = Dash(__name__)
 
 
+#---------------------------------------------------------------#
+#Scatter plot
 app.layout = html.Div([
     html.H4('Interactive scatter plot with Iris dataset'),
     dcc.Graph(id="scatter-plot"),
@@ -15,6 +21,14 @@ app.layout = html.Div([
         marks={0: 'Low', 2.5: 'High'},
         value=[9, 29]
     ),
+    html.H4('Life expentancy progression of countries per continents'),
+    dcc.Graph(id="graph"),
+    dcc.Checklist(
+        id="queen-dropdown",
+        options=placement['queen'],
+        value=["Americas", "Oceania"],
+        inline=True
+    ),
 ])
 
 
@@ -22,7 +36,7 @@ app.layout = html.Div([
     Output("scatter-plot", "figure"), 
     Input("range-slider", "value"))
 def update_bar_chart(slider_range):
-    df = pd.read_csv("dash_data\placements.csv")
+    df = placement
     low, high = slider_range
     mask = (df['score'] > low) & (df['score'] < high)
     fig = px.scatter(
@@ -31,5 +45,22 @@ def update_bar_chart(slider_range):
         hover_data=['score'])
     return fig
 
+
+#---------------------------------------------------------------#
+#Chart
+
+episodes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14']
+
+melted = pd.melt(scores, id_vars='queen', value_vars=episodes)
+
+@app.callback(
+    Output("graph", "figure"), 
+    Input("queen-dropdown", "value"))
+def update_line_chart(queens):
+    df = melted
+    mask = df.queen.isin(queens)
+    fig = px.line(df[mask], 
+        x="variable", y="value", color='queen')
+    return fig
 
 app.run_server(debug=True)
