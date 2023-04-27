4e8cd133c7377e5a468cca5c31008291b4602bf9
Anton Reyes <anton_reyes@dlsu.edu.ph>
2023-04-27 18:31:50 +0800

    plot finally works

--- dashboard.py
+++ dashboard.py
@@ -1,29 +1,35 @@
-import dash
-from dash import html
-from dash import dcc
-import dash_bootstrap_components as dbc
-import pandas as pd
+from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
-from dash.dependencies import Input, Output
+import pandas as pd
 
-# load data
-df1 = pd.read_csv('dash_data/placements.csv')
-#df2 = pd.read_csv('data_data/scores.csv')
+app = Dash(__name__)
 
-# make plot
 
-# initialize app
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.UNITED])
-# set app layout
-
-html.Div([
-    html.H1('Hello Dash'),
-    html.Div([
-        html.P('This is a paragraph.')
-    ])
+app.layout = html.Div([
+    html.H4('Interactive scatter plot with Iris dataset'),
+    dcc.Graph(id="scatter-plot"),
+    html.P("Filter by petal width:"),
+    dcc.RangeSlider(
+        id='range-slider',
+        min=4, max=41, step=5,
+        marks={0: 'Low', 2.5: 'High'},
+        value=[9, 29]
+    ),
 ])
 
 
+@app.callback(
+    Output("scatter-plot", "figure"), 
+    Input("range-slider", "value"))
+def update_bar_chart(slider_range):
+    df = pd.read_csv("dash_data\placements.csv")
+    low, high = slider_range
+    mask = (df['score'] > low) & (df['score'] < high)
+    fig = px.scatter(
+        df[mask], x="score", y="win", 
+        color="w/r/e", size='score',
+        hover_data=['score'])
+    return fig
 
-if __name__ == "__main__":
-    app.run_server(debug=True)
+
+app.run_server(debug=True)
