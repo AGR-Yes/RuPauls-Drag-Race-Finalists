6eb964e61e26c4a016b0f7852d7ce5eb8258c228
Anton Reyes <anton_reyes@dlsu.edu.ph>
2023-04-30 20:35:53 +0800

    updatye

--- dashboard.py
+++ dashboard.py
@@ -63,6 +63,8 @@ melted = pd.read_csv('dash_data\melted_scores.csv')
 
 episodes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14']
 
+options = [{'label': queen, 'value': queen} for queen in placement['queen'].unique()]
+
 all_options = dict(zip(melted['code'], melted['queen']))
 #---------------------------------------------------------------#
 
@@ -74,43 +76,38 @@ graphstyle ={'width': '70vw',
 
 #---------------------------------------------------------------#
 #GRAPHS PLOTS
-content = html.Div([
-#SCATTERPLOT    
-
-
-    html.H4('Interactive scatter plot with Iris dataset'),
-    dcc.Graph(id="scatter-plot",
-              responsive='auto',
-              style=graphstyle),
-    html.P("Filter by petal width:"),
-    dcc.RangeSlider(
-        id='range-slider',
-        min=4, max=41, step=5,
-        marks={0: 'Low', 2.5: 'High'},
-        value=[9, 29],
-        verticalHeight=900,
-        ),
-
-
-#LINE CHART
-    
-    
-html.Div(
-    html.H4('Life expentancy progression of countries per continents'),
-    dcc.Graph(id="graph",
-              responsive='auto',
-              style=graphstyle),
-    dcc.Checklist(
-        id="queen-dropdown",
-        options=melted['queen'].unique(),
-        value=['Sasha Colby'],
-        inline=True,
-        ),
-    sidebar,
+content = dbc.Container(
+    [
+        dbc.Row(
+            [
+                dbc.Col(
+                    dcc.Graph(id="scatter-plot", responsive='auto',style=graphstyle),
+                ),
+                html.Div(
+                    dcc.RangeSlider(
+                        id='range-slider',
+                        min=4, max=41, step=5,
+                        marks={0: 'Low', 2.5: 'High'},
+                        value=[9, 29],
+                        verticalHeight=900
+                    )
+                ),
+                dcc.Graph(id="scatter-plot", responsive='auto',style=graphstyle),
+    html.Div(
+        dcc.RangeSlider(
+            id='range-slider',
+            min=4, max=41, step=5,
+            marks={0: 'Low', 2.5: 'High'},
+            value=[9, 29],
+            verticalHeight=900
+        )
+                ),
+                sidebar
+            ]
+        )
+    ]
 )
-])
-
-
+    
 
 #---------------------------------------------------------------#
 #APP LAYOUT
@@ -121,8 +118,8 @@ app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 #CALLBACK
 #SCATTERPLOT
 @app.callback(
-    Output("scatter-plot", "figure"), 
-    Input("range-slider", "value"))
+    dash.dependencies.Output("scatter-plot", "figure"), 
+    [dash.dependencies.Input("range-slider", "value")])
 def update_bar_chart(slider_range):
     df = placement
     low, high = slider_range
