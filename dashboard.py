bd7bd1e88013a2461f0fc1964c51ba505a67ac22
Anton Reyes <anton_reyes@dlsu.edu.ph>
2023-05-01 20:42:37 +0800

    Table is layouted and functioning

--- dashboard.py
+++ dashboard.py
@@ -5,6 +5,10 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 
+import requests
+from bs4 import BeautifulSoup
+
+
 #---------------------------------------------------------------#
 #DATAFRAMES DF DATASETS 
 
@@ -16,9 +20,9 @@ placement_col = ['bottom', 'low', 'safe', 'high', 'semiwin', 'win']
 
 placement = placement.drop(columns=['Unnamed: 0'])
 
-table_header = [html.Th(col) for col in placement.columns]
+#table_header = [html.Th(col) for col in placement.columns]
 
-table_body = [html.Tr([html.Td(placement.iloc[i][col]) for col in placement.columns]) for i in range(len(placement))]
+#table_body = [html.Tr([html.Td(placement.iloc[i][col]) for col in placement.columns]) for i in range(len(placement))]
 
 
 #---------------------------------------------------------------#
@@ -59,6 +63,12 @@ CONTENT_STYLE = {
     'backgroundColor':'#faeeeb',
 }
 
+#HEADER STYLE
+HEADER_STYLE = {
+    "textAlign":"center"
+
+}
+
 #---------------------------------------------------------------#
 #APP INITIALIZATION
 app = Dash(__name__, external_stylesheets=[dbc.themes.VAPOR])
@@ -67,14 +77,19 @@ server = app.server
 #HEADER
 
 header = html.Div([
-    html.H1("You're a Winner, Baby!"),
-    html.H2("A RuPaul's Drag Race Dashboard for Finalists"),
-])
+    html.H1("You're a Winner, Baby!", style = HEADER_STYLE),
+    html.H2("A RuPaul's Drag Race Dashboard for Finalists", style = HEADER_STYLE),
+    ], className = "mt-4"
+)
 
 #---------------------------------------------------------------#
 #FOOTER
 
-footer = html.Div()
+footer = html.Div(
+    html.H2('FOOTER.'), 
+    style={'position': 'relative', 'bottom': 0, 'width': '100%', 'background-color': '#f8f9fa', 'text-align': 'center', 'padding': '10px'}
+    )
+
 
 #---------------------------------------------------------------#
 #GRAPHS PLOTS
@@ -206,27 +221,54 @@ content = dbc.Container(
 
 #INFO TABLE
         dbc.Row(
-            [
+            [           html.H2('Queen Placements'),
+                dbc.Col(
+                    [
+                        
+                        html.H3("Instructions:",
+                               style={'font-weight': 'bold'},
+                               className="mt-4"),
+                        html.P(
+                            """ 
+                               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a est consequat, posuere purus sed, pharetra nulla. 
+                               Morbi tempus eget mi lacinia semper. Quisque ut laoreet risus, quis semper urna. Nam quis tristique dolor. 
+                               Nunc efficitur consequat aliquam. Aliquam erat volutpat. Pellentesque sit amet laoreet dui. 
+                               Morbi id sapien sed sem tempor dignissim eget in neque. Sed sapien lacus, ornare a cursus et, commodo a tortor. 
+                               Sed vel ipsum consectetur, vestibulum ipsum id, dictum elit. 
+                               Nulla tempor, lacus vel elementum ultrices, arcu diam scelerisque augue, quis consequat elit tellus non ante. 
+                               Aliquam magna quam, vestibulum sit amet tempus congue, molestie ultrices sem.
+                               """,
+                               className="mt-4"),
+
+                    ], width = 5
+                ),
+                
                 dbc.Col(
                     [
 
-                        html.H2('Queen Placements'),
                         dcc.Dropdown(
                             id='row-dropdown',
                             options=[{'label': row['queen'], 'value': str(row.name)} for _, row in placement.iterrows()],
                             placeholder='Select a row...'
                         ),
-                        html.Table([
+                        dbc.Table(
+                            [
                             # Table header
                             html.Thead(
                                 html.Tr([html.Th(col) for col in placement.columns])
                             ),
                             # Table body
-                            html.Tbody(id='table-body')
-                        ])
-                    ]
-                )
-            ]
+                            html.Tbody(id='table-body')],      
+                            bordered=True,
+                            dark=True,
+                            hover=True,
+                            responsive=True,
+                            striped=True,
+                        ),
+                    ], width = 7
+                ),
+
+            ], className="mb-4",
         )
     ]
 )
@@ -234,7 +276,7 @@ content = dbc.Container(
 
 #---------------------------------------------------------------#
 #APP LAYOUT
-app.layout = html.Div([header, content])
+app.layout = html.Div([header, content, footer])
 
 #---------------------------------------------------------------#
 #APP CALLBACKS
@@ -269,15 +311,15 @@ def update_line_chart(queens):
 )
 def update_table(selected_row):
     if selected_row is not None and selected_row != 'Unnamed: 0':
-        row_num = int(selected_row)
+        queen_name = placement.loc[int(selected_row), 'queen']
+        queen_rows = placement.loc[placement['queen'] == queen_name]
         return [
             html.Tr([
-                html.Td(data) for data in placement.iloc[row_num]
-            ])
+                html.Td(data) for data in queen_rows.iloc[i]
+            ]) for i in range(len(queen_rows))
         ]
     else:
         return []
-
 
 
 #---------------------------------------------------------------#
