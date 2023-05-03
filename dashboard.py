8fc6ffe202bab75011a08d96b297b30d4a5f4e02
Anton Reyes <anton_reyes@dlsu.edu.ph>
2023-05-03 22:58:40 +0800

    added write ups and updated layout

--- dashboard.py
+++ dashboard.py
@@ -16,10 +16,26 @@ placement_col = ['bottom', 'low', 'safe', 'high', 'semiwin', 'win']
 
 placement = placement.drop(columns=['Unnamed: 0'])
 
-#table_header = [html.Th(col) for col in placement.columns]
+#---------------------------------------------------------------#
+#Tables
+#SCATTERPLOT TABLE
+title = pd.DataFrame({
+    "Letter": ["W", "R", "E"],
+    "Title": ["Winner", "Runner Up", "Eliminated"],
+    "Meaning": ["Queen won the season", "Queen was the runner up of the season", "Queen was eliminated during the finale"]
+})
 
-#table_body = [html.Tr([html.Td(placement.iloc[i][col]) for col in placement.columns]) for i in range(len(placement))]
-
+#LINECHART TABLE
+linechart = pd.DataFrame({
+    "Values":["0","1","2","3","4","5"],
+    "Placement":["Bottom","Low","Safe","High","Semi-Win","Win"],
+    "Meaning:":["Landed in bottom 2 and lipsynced",
+                "Bad critiques, but was safe",
+                "Safe in the episode",
+                "Good critiques, but did not win the challenge",
+                "Was in the top 2, but did not win the lipsync (All-Stars and select episodes)",
+                "Won the episode; Or in All-Stars, won the top 2 lipsync"]
+})
 
 #---------------------------------------------------------------#
 #STYLING
@@ -40,17 +56,6 @@ external_stylesheets = [
      }
 ]
 
-#SIDEBAR STYLE
-SIDEBAR_STYLE = {
-    "position": "fixed",
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
-    "width": "18rem",
-    "padding": "2rem 1rem",
-    "background-color": "#fdf9de",
-}
-
 #CONTENT STYLE
 CONTENT_STYLE = {
     "margin-left": "18rem",
@@ -63,13 +68,17 @@ CONTENT_STYLE = {
 HEADER_STYLE = {
     "textAlign":"center",
     "margin-bottom":"25px",
+    'background-color': '#1f2630', 
+    'text-align': 'center', 
+    'padding': '4px'
 
 }
 
 #---------------------------------------------------------------#
 #APP INITIALIZATION
-app = Dash(__name__, external_stylesheets=[dbc.themes.VAPOR])
+app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 server = app.server
+
 #---------------------------------------------------------------#
 #HEADER
 
@@ -83,46 +92,53 @@ header = html.Div([
 #FOOTER
 
 footer = html.Div(
-    html.H2('FOOTER.'), 
-    style={'position': 'relative', 'bottom': 0, 'width': '100%', 'background-color': '#1f2630', 'text-align': 'center', 'padding': '10px'}
+    html.A("Github", "https://github.com/agr-Yes/"), 
+    style={'position': 'relative', 
+           'bottom': 0, 
+           'width': '100%', 
+           'background-color': '#1f2630', 
+           'text-align': 'center', 
+           'padding': '5px'}
     )
 
-
 #---------------------------------------------------------------#
-#GRAPHS PLOTS
+#CONTENT
 content = dbc.Container(
     [
-#opening text
+#OPENING
         dbc.Row(
             [
                 dbc.Col(
                     html.Div(
                         """ 
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a est consequat, posuere purus sed, pharetra nulla. 
-                            Morbi tempus eget mi lacinia semper. Quisque ut laoreet risus, quis semper urna. Nam quis tristique dolor. 
-                            Nunc efficitur consequat aliquam. Aliquam erat volutpat. Pellentesque sit amet laoreet dui. 
-                            Morbi id sapien sed sem tempor dignissim eget in neque. Sed sapien lacus, ornare a cursus et, commodo a tortor. 
-                            Sed vel ipsum consectetur, vestibulum ipsum id, dictum elit. 
-                            Nulla tempor, lacus vel elementum ultrices, arcu diam scelerisque augue, quis consequat elit tellus non ante. 
-                            Aliquam magna quam, vestibulum sit amet tempus congue, molestie ultrices sem.
+"RuPaul's Drag Race" is a reality tv show casting talented drag queens every season to compete in challenges in attempt to make it to the top and be America's Next Drag Superstar and earn the cash prize.
+            
+Each episode consists of a main challenge and a themed runway show. Every week, one queen is declared the winner of an episode based on their performance, while two others are announced to be up for elimination. The bottom two must then compete to stay on the show with a lip-sync for their lives.
+
+Expanding from the United States, the Drag Race franchises has reached out to other continents such as Europe and Asia; Making more queen declared as a 'Ru-girl' and potentially invited back to compete in an All-Star or International season.
+
+This dashboard shows the data of the finalists in all franchises and countries (as of May 1, 2023). To see more, scroll down to see how your favorite finalist did!
                             """,
-                            style = {"border": "1px white solid",
+                            style = {"border": "1px",
                                      "padding": "10px",
-                                     }),
-                    width = 4,
+                                     'whiteSpace': 'pre-wrap',
+                                     }
+                        ),
+                    width = 9,
                 ),
                 dbc.Col(
-                    html.Div(
-                        """ 
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a est consequat, posuere purus sed, pharetra nulla. 
-                            Morbi tempus eget mi lacinia semper. Quisque ut laoreet risus, quis semper urna. Nam quis tristique dolor. 
-                            Nunc efficitur consequat aliquam. Aliquam erat volutpat. Pellentesque sit amet laoreet dui. 
-                            Morbi id sapien sed sem tempor dignissim eget in neque. Sed sapien lacus, ornare a cursus et, commodo a tortor. 
-                            Sed vel ipsum consectetur, vestibulum ipsum id, dictum elit. 
-                            Nulla tempor, lacus vel elementum ultrices, arcu diam scelerisque augue, quis consequat elit tellus non ante. 
-                            Aliquam magna quam, vestibulum sit amet tempus congue, molestie ultrices sem.
-                            """),
-                    width = 8,
+                    html.Div([
+                        html.H4("Contact me:"),
+                        html.A("Instagram", href="https://www.instagram.com/ant0nreyes/"), html.Br(),
+                        html.A("Github", href="https://github.com/AGR-Yes/"), html.Br(),
+                        html.A("LinkedIn", href="https://www.linkedin.com/in/antongreyes/"), html.Br(),
+                        html.H5("Project Repository"), html.Br(),
+                        html.A("You're A Winner, Baby!", href="https://github.com/AGR-Yes/RuPauls-Drag-Race-Finalists"), html.Br(),
+                        ]), style = {"border": "1px white solid",
+                                     "padding": "10px",
+                                     'whiteSpace': 'pre-wrap',
+                                     },
+                    width = 3,
                 ),
 
             ],
@@ -153,20 +169,26 @@ content = dbc.Container(
                             value = 'win',
                             className = 'dropdown2 mb-4',                            
                         ),
-                        html.H3("Instructions:",
+                        html.H4("Instructions:",
                                style={'font-weight': 'bold'},
                                className="mt-4"),
                         html.P(
                             """ 
-                               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a est consequat, posuere purus sed, pharetra nulla. 
-                               Morbi tempus eget mi lacinia semper. Quisque ut laoreet risus, quis semper urna. Nam quis tristique dolor. 
-                               Nunc efficitur consequat aliquam. Aliquam erat volutpat. Pellentesque sit amet laoreet dui. 
-                               Morbi id sapien sed sem tempor dignissim eget in neque. Sed sapien lacus, ornare a cursus et, commodo a tortor. 
-                               Sed vel ipsum consectetur, vestibulum ipsum id, dictum elit. 
-                               Nulla tempor, lacus vel elementum ultrices, arcu diam scelerisque augue, quis consequat elit tellus non ante. 
-                               Aliquam magna quam, vestibulum sit amet tempus congue, molestie ultrices sem.
+Select from the dropdown list the placement you would want so see. 
+The plot will adjust accordingly based on the placement selected. 
+The scores show the score a contestant got by the end of their season. 
+You can also click on the w/r/e on the graph's side, to show or hide specific placements.                               
                                """,
-                               className="mt-4"),
+                               className="mt-4"), html.Br(),
+                        html.P(
+                            """
+By hovering on each dot, you can find out which contestant it pertains to.                            
+                        """),
+                        dbc.Table([
+                            html.Thead(html.Tr([html.Th(col) for col in title.columns])), 
+                            html.Tbody([html.Tr([html.Td(title.iloc[i][col]) for col in title.columns]) for i in range(len(title))])   
+                            ],
+                            bordered=True,)                             
 
                     ], width = 5
                 )
@@ -177,20 +199,13 @@ content = dbc.Container(
         dbc.Row(
             [
                         html.H2('Final Progress per Finalist', style = HEADER_STYLE),
-                dbc.Col(
-                    [
-                        
                         dcc.Graph(id="graph"),
-                        
-                    ], width = 7
-                ),
-
                 dbc.Col(
                     [
-
+                        
                         html.P("Select the queens you want to compare",
                                style={'font-weight': 'bold'},
-                               className="mb-4"),
+                               className="mt-4 mb-4"),
                         dcc.Dropdown(
                             id='queen-dropdown',
                             options=[{'label': queen, 'value': queen} for queen in melted['queen'].unique()],
@@ -199,23 +214,41 @@ content = dbc.Container(
                             style={'width': '100%', 'margin': 'auto'},
                             className = 'dropdown1 mt-4',
                         ),
-                        html.H3("Instructions:",
+                        html.H4("Instructions:",
                                style={'font-weight': 'bold'},
                                className="mt-4"),
                         html.P(
                             """ 
-                               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a est consequat, posuere purus sed, pharetra nulla. 
-                               Morbi tempus eget mi lacinia semper. Quisque ut laoreet risus, quis semper urna. Nam quis tristique dolor. 
-                               Nunc efficitur consequat aliquam. Aliquam erat volutpat. Pellentesque sit amet laoreet dui. 
-                               Morbi id sapien sed sem tempor dignissim eget in neque. Sed sapien lacus, ornare a cursus et, commodo a tortor. 
-                               Sed vel ipsum consectetur, vestibulum ipsum id, dictum elit. 
-                               Nulla tempor, lacus vel elementum ultrices, arcu diam scelerisque augue, quis consequat elit tellus non ante. 
-                               Aliquam magna quam, vestibulum sit amet tempus congue, molestie ultrices sem.
+
+Select from the dropdown list the queen you would want so see. 
+This plot is capable of showing more than one queen at the same time, so just keep searching, adding, or removing as you want.
                                """,
                                className="mt-4"),
+                        html.Br(),
+                        html.P("""
+In the y-axis, you can see the episodes from the lowest value (bottom placement) to the highest value possible (win placement). On the x-axis, you'll be able to see the number of episodes each contestant was a part of during their respective season. 
+
+Since you can input multiple queens at the same time, you can compare multiple queens at the same time.
+                        """),
+                        
+                    ], width = 7
+                ),
+
+                dbc.Col(
+                    [
+
+                        dbc.Table([
+                            html.Thead(html.Tr([html.Th(col) for col in linechart.columns])), 
+                            html.Tbody([html.Tr([html.Td(linechart.iloc[i][col]) for col in linechart.columns]) for i in range(len(linechart))])   
+                            ],
+                            bordered=True,
+                            className="mt-4")  
 
                     ], width = 5
-                )
+                ),
+
+                        
+
             ], className="mt-4 mb-4",
         ),
 
@@ -225,18 +258,19 @@ content = dbc.Container(
                 dbc.Col(
                     [
                         
-                        html.H3("Instructions:",
+                        html.H4("Instructions:",
                                style={'font-weight': 'bold'},
-                               className="mt-4"),
+                               className=""),
                         html.P(
                             """ 
-                               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a est consequat, posuere purus sed, pharetra nulla. 
-                               Morbi tempus eget mi lacinia semper. Quisque ut laoreet risus, quis semper urna. Nam quis tristique dolor. 
-                               Nunc efficitur consequat aliquam. Aliquam erat volutpat. Pellentesque sit amet laoreet dui. 
-                               Morbi id sapien sed sem tempor dignissim eget in neque. Sed sapien lacus, ornare a cursus et, commodo a tortor. 
-                               Sed vel ipsum consectetur, vestibulum ipsum id, dictum elit. 
-                               Nulla tempor, lacus vel elementum ultrices, arcu diam scelerisque augue, quis consequat elit tellus non ante. 
-                               Aliquam magna quam, vestibulum sit amet tempus congue, molestie ultrices sem.
+From the dropdown menu, select the finalist you want to view.
+                               """,
+                               className="mt-4"),
+                        html.Br(),
+                        html.P(
+                            """ 
+In the table, will see your selected Queen's season/s they were apart of, score, and number of placements they've accumulated during their run. 
+Also, if you select a Queen that has been a part of multiple seasons (as long as they reached the finale,) you would see the score and placement count as well.
                                """,
                                className="mt-4"),
 
@@ -272,13 +306,13 @@ content = dbc.Container(
     ]
 )
 
-
 #---------------------------------------------------------------#
 #APP LAYOUT
 app.layout = html.Div([header, content, footer])
 
 #---------------------------------------------------------------#
 #APP CALLBACKS
+#SCATTERPLOT
 @app.callback(
     Output("scatter-plot", "figure"), 
     Input("dropdown", "value")
@@ -293,6 +327,7 @@ def update_bar_chart(dropdown_value):
         size = 'score')
     return fig
 
+#LINECHART
 @app.callback( #dropdown for line chart
     Output("graph", "figure"), 
     Input("queen-dropdown", "value"))
@@ -304,6 +339,7 @@ def update_line_chart(queens):
         template=dark_template)
     return fig
 
+#TABLE
 @app.callback(
     Output('table-body', 'children'),
     [Input('row-dropdown', 'value')]
@@ -319,7 +355,6 @@ def update_table(selected_row):
         ]
     else:
         return []
-
 
 #---------------------------------------------------------------#
 #RUN APP
